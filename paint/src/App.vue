72991a6c94fd04d747cd14e1a7a728f05ea5ffcc
ahmed <ahmed@192.168.1.7>
2020-12-29 15:52:00 +0200

    drawing frontend

---
+++ paint/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <Canvas />
+  </div>
+</template>
+
+<script>
+
+import Canvas from "./components/Canvas.vue";
+
+export default {
+  name: "App",
+  components: {
+    Canvas,
+  },
+};
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  background-color: gray;
+}
+</style>
